149dbaf192aacd4330b987e018165d8da965f4c9
mmoni4282 <mmoni4282@example.com>
2026-05-10 14:11:04 +0530

    added env file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Secret Keys
+// Environment Variables
+const REACT_APP_KEY_VERSION = process.env.REACT_APP_KEY_VERSION || '2.0';
+const REACT_APP_KEY_TIME = process.env.REACT_APP_KEY_TIME || '2:00';
+const REACT_APP_KEY_DATE = process.env.REACT_APP_KEY_DATE || '2026-05-10';
+const REACT_APP_KEY_PORTAL = process.env.REACT_APP_KEY_PORTAL || 'STUDENT';
+
 function App() {
   return (
     <div className="App">
